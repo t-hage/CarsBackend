9dc00def5bbeb8afe13afb2081e174889184b361
Tom <tommhage@gmail.com>
2018-02-27 20:15:47 +0100

    removed unnecessary order on resourceconfig class

--- src/main/scala/com/tom/config/AppResourceConfig.scala
+++ src/main/scala/com/tom/config/AppResourceConfig.scala
@@ -2,7 +2,6 @@ package com.tom.config
 
 import javax.servlet.ServletContext
 
-import org.springframework.core.annotation.Order
 import org.springframework.web.WebApplicationInitializer
 import org.springframework.web.context.ContextLoaderListener
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
@@ -10,7 +9,6 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 /**
   * Created by Tom on 22-Feb-18.
   */
-@Order(1)
 class AppResourceConfig() extends WebApplicationInitializer {
 
   override def onStartup(servletContext: ServletContext): Unit = {
